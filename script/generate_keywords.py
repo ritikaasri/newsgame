13252e964c5da08a2fadf79ee735d9454cf1422e
lileungws <146325596+lileungws@users.noreply.github.com>
2024-03-20 00:41:20 +0800

    Update generate_keywords.py
    Amended prompt

--- script/generate_keywords.py
+++ script/generate_keywords.py
@@ -7,7 +7,7 @@ df = pd.read_csv('web/data/main.csv')
 keywords = []
 for index, row in df.iterrows():
     title = row['title']
-    prompt = f"Extract 4 separate single words which are most important in the given title which are not small words in \"{title}\""
+    prompt = f"Extract 4 sperated single words which are most important in the given title which are not small words in \"{title}\", the keywords should have no punctuation and all in lower case. "
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": prompt}],
